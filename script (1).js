3e9bf7183ad726ad52fd5e10946a14339d739ef6
Bloblime <150110732+Bloblime@users.noreply.github.com>
2023-11-12 15:09:09 +0800

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,37 @@
+//function name must have a small letter for first word and big letter for second
+
+function toAbout(){
+    window.location="about us.html";
+}
+
+function toGallery(){
+    window.location = "GALLERY_PAGE.html";
+}
+
+function toEarrings(){
+    window.location = "Earrings.html";
+}
+
+function toBracelets(){
+    window.location = "Bracelets.html";
+}
+
+function toWatch(){
+    window.location = "Watch.html";
+}
+
+function toRings(){
+    window.location = "Rings.html";
+}
+
+function toPendants(){
+    window.location = "Pendants.html";
+}
+
+function toNecklace(){
+    window.location = "Necklace.html";
+}
+
+function toIndex(){
+    window.location = "Index.html";
+}
