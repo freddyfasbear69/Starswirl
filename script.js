af309bbb76142ce9e0592256d8e18f21832e59a4
Bloblime <150110732+Bloblime@users.noreply.github.com>
2023-11-14 20:55:04 +0800

    www

--- script.js
+++ script.js
@@ -36,9 +36,10 @@ function toIndex(){
     window.location = "Index.html";
 }
 
-.function cart(){
-    
-
-
-
+function cart(){
+    if (confirm("Add this item to your cart?") == true) {
+        alert("Successfully added to cart!");
+        } else {
+        alert("This item was not added to your cart.");
+        }
 }
